882d16e60f766c133a739684d46c1fdb12b9ce35
Khylko Andrii <hilko_a@itstep.academy>
2020-09-23 18:35:55 +0300

    Add calc

--- main.cpp
+++ main.cpp
@@ -9,4 +9,25 @@ using namespace std;
 int main()
 {
 	cout<<"Hello"<<endl;
+	int a, b;
+	cout<<"Enter two numbers->";
+	cin>>a>>b;
+	char op;
+	cout<<"Enter the operation(+, -, *, /)->";
+	cin>>op;
+	switch(op)
+	{
+		case '+':
+		cout<<Sum(a,b)<<endl;
+		break;
+		case '-':
+		cout<<Dif(a,b)<<endl;
+		break;
+		case '*':
+		cout<<Prod(a,b)<<endl;
+		break;
+		case '/':
+		cout<<Div(a,b)<<endl;
+		break;
+	}
 }
